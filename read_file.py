be03dc62ab8c6212ea1ff2525c7f1aa85a87a01e
bipashaghosal <66522378+bipashaghosal@users.noreply.github.com>
2020-06-12 22:34:49 +0530

    Add files via upload

---
+++ read_file.py
@@ -0,0 +1,8 @@
+def read_file(file_name):
+    opened_file = open(file_name, 'r')
+    lines_list = []
+    for line in opened_file:
+        line = line.split()
+        lines_list.append(line)
+
+    return lines_list
